d6cdad0e6c3186cac1966d5ad407b0505fc4000f
Morkeltry <morkeltry@mailfence.com>
2017-08-10 16:08:41 +0100

    Added 'loggedin' route to routers.js, which checks existence and validity of jwt, and passes a file-serving cb to checkLogin in validator.js F**xxes #31

---
+++ src/validator.js
@@ -0,0 +1,9 @@
+function checkLogin (strippedJwt, successCallback) {
+/// performs query to validate strippedJwt contents,
+// passes (err, user) back to cb, where user = display name of user who is OK to log in.
+/// this is where we would pass back {id,user} if id required in frontend
+
+
+
+// const strippedJwt = {jwtContents.username, jwtContents.hashPassword,  jwtContents.perUserSalt}
+}
