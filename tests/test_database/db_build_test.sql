15aca58334bb251c40455ab26a9d95a929fe6f49
RogeredBacon <jinglis12@googlemail.com>
2017-08-10 11:02:46 +0100

    Added the test database files

---
+++ tests/test_database/db_build_test.sql
@@ -0,0 +1,26 @@
+BEGIN;
+
+DROP TABLE IF EXISTS users, list CASCADE;
+
+DROP TABLE IF EXISTS list CASCADE;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(20) NOT NULL,
+  hashedPassword VARCHAR(100) NOT NULL,
+  is_admin BOOLEAN DEFAULT false
+);
+
+INSERT INTO users (username, hashedPassword, is_admin) VALUES ('Tom', 12345, false), ('James', 6789, true), ('Amelie', 10101, false);
+
+CREATE TABLE list (
+  id SERIAL PRIMARY KEY,
+  owner_id INTEGER REFERENCES users (id),
+  content TEXT NOT NULL,
+  marked_by INTEGER REFERENCES users (id)
+);
+
+INSERT INTO list (owner_id, content, marked_by) VALUES (1, 'get milk', 2), (2, 'buy flowers', 2), (1, 'buy cookies', null);
+
+
+COMMIT;
